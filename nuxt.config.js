bbcb2bf19f69cbd7d1d94328120f4474d2ced611
arfan <ayogaan@gmail.com>
2022-06-29 16:39:28 +0700

    fix icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'model-kwu',
+    title: 'olah-dataku',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'id'
     },
     meta: [
       { charset: 'utf-8' },
